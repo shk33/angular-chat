e4a6b520dbe707fb148f4720823f65f44cc4bf40
Miguel Coronel <m.coronel.seg@gmail.com>
2015-04-25 00:26:57 -0500

    Add title to the chat and turn off feed function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,10 +12,15 @@ angular.module('angularApp')
   .controller('MainCtrl', function ($scope, $timeout) {
     var rootRef  = new Firebase('https://glowing-fire-3651.firebaseio.com/');
     var messagesRef = rootRef.child('messages');
+    var titleRef = rootRef.child('title');
 
     $scope.currentUser = null;
     $scope.currentText = null;
     $scope.messages    = [];
+
+    titleRef.once('value',function(snapshot) {
+      $scope.title = snapshot.val();
+    });
 
     messagesRef.on('child_added',function(snapshot) {
       $timeout(function () {
@@ -72,6 +77,10 @@ angular.module('angularApp')
       };
 
       messagesRef.push(newMessage);
+    };
+
+    $scope.turnFeedOff = function () {
+      messagesRef.off();
     };
 
   });
