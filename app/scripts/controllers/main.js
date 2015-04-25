c330f540bf9ff903a19edbe9d3a6d1c3f67a6f06
Miguel Coronel <m.coronel.seg@gmail.com>
2015-04-25 00:07:37 -0500

    Update in realtime the messages adding the event 'child_changed'

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,4 @@
+/*global Firebase*/
 'use strict';
 
 /**
@@ -9,18 +10,44 @@
  */
 angular.module('angularApp')
   .controller('MainCtrl', function ($scope, $timeout) {
-    var rootRef  = new Firebase("https://glowing-fire-3651.firebaseio.com/");
+    var rootRef  = new Firebase('https://glowing-fire-3651.firebaseio.com/');
     var messagesRef = rootRef.child('messages');
 
     $scope.currentUser = null;
     $scope.currentText = null;
+    $scope.messages    = [];
 
-    messagesRef.on('value',function(snapshot) {
+    messagesRef.on('child_added',function(snapshot) {
       $timeout(function () {
         var snapshotVal = snapshot.val();
-        $scope.messages = snapshotVal;
+        $scope.messages.push({
+          text: snapshotVal.text,
+          user: snapshotVal.user,
+          key:  snapshot.key()
+        });
       });
     });
+
+    messagesRef.on('child_changed',function(snapshot) {
+      $timeout(function () {
+        var snapshotVal = snapshot.val();
+        var message = findMessageByKey(snapshot.key());
+        message.text = snapshotVal.text;
+      });
+    });
+
+    function findMessageByKey (key) {
+      var messageFound = null;
+      for (var i = 0; i < $scope.messages.length; i++) {
+        var currentMessage = $scope.messages[i];
+        if (currentMessage.key === key){
+          messageFound = currentMessage;
+          break;
+        }
+      }
+
+      return messageFound;
+    }
 
     $scope.sendMessage = function () {
       var newMessage = {
